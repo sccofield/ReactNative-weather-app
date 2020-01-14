240dcc0b9d432541e7ceb8304aabc78e4fc82b5f
sccofield <michael_umanah@yahoo.com>
2020-01-14 08:52:22 +0100

    add background image based on weather

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, TextInput, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Text, View, Platform, TextInput, KeyboardAvoidingView, ImageBackground, } from 'react-native';
 
 import SearchInput from './components/SearchInput';
+
+import getImageForWeather from './utils/getImageForWeather';
 
 export default function App() {
   return (
     <KeyboardAvoidingView style={styles.container} behaviour='padding'>
-      <Text style={[styles.largeText, styles.textStyle]}>
-        Lagos
-      </Text>
-      <Text style={[styles.smallText, styles.textStyle]}>
-        Light Cloud
-      </Text>
-      <Text style={[styles.largeText, styles.textStyle]}>
-        24°
-      </Text>
+      <ImageBackground
+        source={getImageForWeather('Clear')}
+        style={styles.imageContainer}
+        imageStyle={styles.image}
+      >
+        <View style={styles.detailsContainer}>
+          <Text style={[styles.largeText, styles.textStyle]}>
+            Lagos
+          </Text>
+          <Text style={[styles.smallText, styles.textStyle]}>
+            Light Cloud
+          </Text>
+          <Text style={[styles.largeText, styles.textStyle]}>
+            24°
+          </Text>
 
-      <SearchInput placeholder="Search any city"/>
+          <SearchInput placeholder="Search any city"/>
+        </View>
+      </ImageBackground>
     </KeyboardAvoidingView>
   );
 }
@@ -24,13 +34,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#34495E'
   },
   textStyle: {
     textAlign: 'center',
-    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto'
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white'
   },
   largeText: {
     fontSize: 44
@@ -38,4 +47,19 @@ const styles = StyleSheet.create({
   smallText: {
     fontSize: 18
   },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
+  }
 });
