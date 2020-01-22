c966bcc472ea292bf55793b0f97c576d06393b1e
sccofield <michael_umanah@yahoo.com>
2020-01-22 15:33:22 +0100

    add proptypes for prop validations

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -62,3 +62,11 @@ const styles = StyleSheet.create({
     color: 'white'
   }
 })
+
+SearchInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+};
+SearchInput.defaultProps = {
+  placeholder: '',
+};
